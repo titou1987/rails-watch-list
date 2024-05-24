a2bc1cf3c5748275572598da2a41d87874b9732f
Julien Paris <parisjulien87@gmail.com>
2024-05-24 16:53:11 +0200

    exo 2 24

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem "autoprefixer-rails"
 gem "font-awesome-sass", "~> 6.1"
 gem "simple_form"
 gem "sassc-rails"
-
+gem "cloudinary"
 
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -21,9 +21,13 @@ class ListsController < ApplicationController
     end
   end
 
+  def article_params
+    params.require(:article).permit(:title, :body, :photo)
+  end
+
 private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
